8cd469089528760a3ac886a85d945e98f4743cb6
celi19 <araceligopar@student.hartnell.edu>
2015-04-23 18:33:55 -0700

    changes to robot.cpp

--- MazeProject/Robot.cpp
+++ MazeProject/Robot.cpp
@@ -1,26 +1,38 @@
-#include "Robot.h
+/*
+*************************************************************
+    filename:Robot.cpp
+    Class Name: Robot
+    Author: Araceli Gopar, Anita Garcia, Ana Perez
+    Date: 4/18/2015
+    Compiler: code blocks 13.12
+    Description: Robots class will mimic a robot finding its way out of maze.
+                 Objects of type robot will be able to choose the direction in where to more.
+                 This class will use another class Maze and Levels.
+**************************************************************
+*/
+
+#include "Robot.h"
 #include "Maze.h"
 #include <cassert>
 
-#include <iostream
+#include <iostream>
 using namespace std;
 
 Robot::Robot()
 {
    // Maze check_wall;
+    //Maze maze_grid;
     x_position, y_position = 0;
     robot = 'x';
+    //mazeGrid[0][0] = Maze(robot);
 }
-bool Robot::isWall(int x_position, int y_position)
+bool Robot::isFinished(int x_position, int y_position)
 {
-
-}
-bool Robot::isFinish(int x_position, int y_position)
-{
-
+    return(x_position == 50 && y_position == 50)
 }
 void Robot::moveLeft()
 {
+
 
 }
 void Robot::moveRight()
--- MazeProject/Robot.h
+++ MazeProject/Robot.h
@@ -1,3 +1,16 @@
+/*
+*************************************************************
+    filename:Robot.cpp
+    Class Name: Robot
+    Author: Araceli Gopar, Anita Garcia, Ana Perez
+    Date: 4/18/2015
+    Compiler: code blocks 13.12
+    Description: Robots class will mimic a robot finding its way out of maze.
+                 Objects of type robot will be able to choose the direction in where to more.
+                 This class will use another class Maze and Levels.
+**************************************************************
+*/
+
 #ifndef ROBOT_H_INCLUDED
 #define ROBOT_H_INCLUDED
 
@@ -5,17 +18,61 @@ class Robot
 {
 
     private:
+        Maze mazeGrid;
         int x_position, y_position;
         char robot;
 
     public:
         Robot();
+        //*********************************************************
+        //Summary: Constructor, will initialize an object's variables.
+        //condition:
+        // Precondition:
+        //*********************************************************
         bool isWall(int x_position, int y_position);
-        bool isFInish(int x_position, int y_position);
+        //*********************************************************
+        //Summary: isWall will return true or false whether there is
+        //         char (a wall) in the direction of where the robot
+        //         wants to move.
+        //condition:
+        // Precondition:
+        //*********************************************************
+
+        bool isFinished(int x_position, int y_position);
+        //*********************************************************
+        //Summary: isFinish will return true or false whether the robot
+        //         has completed the maze and is ready to more to the next level.
+        //condition:
+        // Precondition:
+        //*********************************************************
         void moveLeft();
+        //*********************************************************
+        //Summary: moveLeft will move the robot to the left by decreasing
+        //         y's coordinates.
+        //condition:
+        // Precondition:
+        //*********************************************************
         void moveRight();
+        //*********************************************************
+        //Summary: moveRight will move the robot to the right by
+        //         increasing y's coordinates.
+        //condition:
+        // Precondition:
+        //*********************************************************
         void moveUp();
+        //*********************************************************
+        //Summary: moveUp will move the robot up by decreasing
+        //         x's coordinates.
+        //condition:
+        // Precondition:
+        //*********************************************************
         void moveDown();
+        //*********************************************************
+        //Summary: moveDown will move the robot down by increasing
+        //         x's coordinates.
+        //condition:
+        // Precondition:
+        //*********************************************************
 
 };
 #endif // ROBOT_H_INCLUDED
