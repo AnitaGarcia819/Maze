79f5bc0337b160c10933e277a56c6b74c85d56cc
Anita Garcia <anitagarcia819@gmail.com>
2015-05-04 09:56:55 -0700

    Delted windows object code

--- MazeProject/Driver.cpp
+++ MazeProject/Driver.cpp
@@ -6,19 +6,13 @@ using namespace std;
 int main()
 {
    Robot bob;
-
+   char direction;
+    cin >> direction;
+    switch(direction)
+    {
+        case 'H':
+            bob.moveUp();
+    }
     bob.mazeGrid.displayMaze();
-    //bob.mazeGrid.generateLevel(1);
-    //bob.mazeGrid.displayMaze();
-
-    //bob.mazeGrid.displayMaze();
-    //bob.moveDown();
-   // bob.moveUp();
-   // bob.moveRight();
-    //bob.moveLeft();
-    //bob.moveRight();
-    //bob.mazeGrid.displayMaze();
-
-
     return 0;
 }
--- MazeProject/Maze.cpp
+++ MazeProject/Maze.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include "Maze.h"
-#include <iostream>
 
 Maze::Maze()
  {
