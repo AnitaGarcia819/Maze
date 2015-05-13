00985c1ee843c36a3b63c0985b332c5985b0b262
Ana Perez <anacperez@student.hartnell.edu>
2015-05-13 16:42:57 -0700

    generated third maze

--- MazeProject/Maze.cpp
+++ MazeProject/Maze.cpp
@@ -213,6 +213,7 @@ void Maze::generateNewLevel(int level)
                 if((y == 2 || y == 4 || y == 5 || y == 6 || y == 7 || y == 9 || y == 10 || y == 11 || y == 12 || y == 13 || y == 14 || y == 15 || y == 16 || y == 17 || y == 18 || y == 19
                 || y == 21 || y == 23) && (x == 10))
                     maze[x][y] = wall; // row 10
+              //  if((y == ))
             }
         }
     }
